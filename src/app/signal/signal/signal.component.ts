455bf7ab946af8a22e813b75f50fbffe0d28899d
Daniel Ferreira <daniel.alexandre22@hotmail.com>
2024-08-20 22:32:55 -0300

    computed inteligente coisa boa

--- src/app/signal/signal/signal.component.ts
+++ src/app/signal/signal/signal.component.ts
@@ -4,37 +4,41 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-signal',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './signal.component.html',
   styleUrl: './signal.component.css',
 })
 export class SignalComponent {
   protected signalNome = signal('daniel');
   protected signalContador = signal(1);
+  protected showCount = signal(true)
   protected signalBool = signal(false);
-  protected signalObjeto = signal({ nome:"daniel", idade:38 })
+  protected signalObjeto = signal({ nome: 'daniel', idade: 38 });
 
   protected computedExemplo = computed(() => {
-    console.log('passei no computed');
-    if (this.signalContador()) return `${this.signalContador()} computed!`;
-    return null
+    console.log('passou no computed');
+    if (this.showCount()) {
+      return `${this.signalContador()} computed!`;
+    } else {
+      return 'NADA';
+    }
   });
 
-  executarContador(){
-    //set: Modifica o valor
-    // this.signalContador.set(10);
-    this.signalBool.set(true)
-    
-    //update: Modifica o valor com base no valor atual
-    this.signalContador.update(contador => contador + 1)
-
-    //update: Modifica o valor com base no valor atual devendo passar todos os atributos
-    this.signalObjeto.update(obj => {
-      return {...obj, idade:400}
-    })
+  modificarShowConunt(){
+    this.showCount.update(x => !x)
   }
 
-}
+  executarContador() {
+    //set: Modifica o valor
+    // this.signalContador.set(10);
+    this.signalBool.set(true);
 
+    //update: Modifica o valor com base no valor atual
+    this.signalContador.update((contador) => contador + 1);
+
+    //update: Modifica o valor com base no valor atual devendo passar todos os atributos
+    this.signalObjeto.update((obj) => {
+      return { ...obj, idade: 400 };
+    });
+  }
+}
